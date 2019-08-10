01f2d209eba234d916d884ddf930b45183a86eba
Horacio Garcia <garciahor@gmail.com>
2019-08-10 01:46:55 -0400

    need to work on links and projects

--- javascript/main-page.js
+++ javascript/main-page.js
@@ -1,49 +1,68 @@
 $(document).ready(function () {
 
+  $(".navbar-toggler").on("click", function () {
 
+    if ($(".navbar-toggler").attr("aria-expanded") == "false") {
+      $("#navbarToggleExternalContent").attr("class", "collapse.this");
+      $(".navbar-toggler").attr("aria-expanded", "true");
+    } else if ($(".navbar-toggler").attr("aria-expanded") == "true") {
+      $("#navbarToggleExternalContent").attr("class", "collapse");
+      $(".navbar-toggler").attr("aria-expanded", "false");
+    };
 
-    $(".navbar-toggler").on("click", function () {
+  });
 
-        if ($(".navbar-toggler").attr("aria-expanded") == "false") {
-            $("#navbarToggleExternalContent").attr("class", "collapse.this");
-            $(".navbar-toggler").attr("aria-expanded", "true");
-        } else if ($(".navbar-toggler").attr("aria-expanded") == "true") {
-            $("#navbarToggleExternalContent").attr("class", "collapse");
-            $(".navbar-toggler").attr("aria-expanded", "false");
-        };
-
-    });
-
-   $(".flex-sm-fill").on("click", function(){
-    let value =$(this).attr("id");
+  $(".flex-sm-fill").on("click", function () {
+    let value = $(this).attr("id");
     console.log(value);
-if(value=="1")
-{   
-    $( "#1" ).addClass( "active" );
-    $( "#2" ).removeClass( "active" );
-    $( "#3" ).removeClass( "active" );
-    $( "#4" ).removeClass( "active" );
-} else if(value=="2")
-{
-    $( "#1" ).removeClass( "active" );
-    $( "#2" ).addClass( "active" );
-    $( "#3" ).removeClass( "active" );
-    $( "#4" ).removeClass( "active" );
-}else if(value=="3")
-{
-    $( "#1" ).removeClass( "active" );
-    $( "#2" ).removeClass( "active" );
-    $( "#3" ).addClass( "active" );
-    $( "#4" ).removeClass( "active" );
-}else if(value=="4")
-{
-    $( "#1" ).removeClass( "active" );
-    $( "#2" ).removeClass( "active" );
-    $( "#3" ).removeClass( "active" );
-    $( "#4" ).addClass( "active" );
-}
+    if (value == "1") {
+      $("#1").addClass("active");
+      $("#2").removeClass("active");
+      $("#3").removeClass("active");
+      $("#4").removeClass("active");
+    } else if (value == "2") {
+      $("#1").removeClass("active");
+      $("#2").addClass("active");
+      $("#3").removeClass("active");
+      $("#4").removeClass("active");
+    } else if (value == "3") {
+      $("#1").removeClass("active");
+      $("#2").removeClass("active");
+      $("#3").addClass("active");
+      $("#4").removeClass("active");
+    } else if (value == "4") {
+      $("#1").removeClass("active");
+      $("#2").removeClass("active");
+      $("#3").removeClass("active");
+      $("#4").addClass("active");
+    }
 
-   });
+  });
 
 
+  $('.carousel').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 });
